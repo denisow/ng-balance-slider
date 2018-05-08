bd20abab6648ffcb08a49779f73300b09e4dc0b1
Pavel Denisov <DenisovPV@sibedge.com>
2018-05-08 15:26:12 +0700

    remove app module

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { balanceSliderModule } from './balance-slider/balance-slider.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(balanceSliderModule)
   .catch(err => console.log(err));
